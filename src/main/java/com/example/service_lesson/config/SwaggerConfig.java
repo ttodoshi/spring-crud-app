d40ae3618c1a639482c667bfa80964106d80876e
todoshi <ttodoshi@users.noreply.github.com>
2023-05-07 12:59:44 +0300

    add swagger

---
+++ src/main/java/com/example/service_lesson/config/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package com.example.service_lesson.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info().title("Service Lesson").version("v0.1"));
+    }
+}
--- src/main/java/com/example/service_lesson/controller/ThemeController.java
+++ src/main/java/com/example/service_lesson/controller/ThemeController.java
@@ -3,6 +3,10 @@ package com.example.service_lesson.controller;
 import com.example.service_lesson.dto.ThemeDto;
 import com.example.service_lesson.model.Theme;
 import com.example.service_lesson.service.ThemeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,27 +25,77 @@ public class ThemeController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all themes", tags = "theme")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Themes list",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json")
+                    })
+    })
     public ResponseEntity<List<ThemeDto>> getThemes() {
         return ResponseEntity.ok(themeService.getThemes());
     }
 
     @GetMapping(path = "{themeId}")
+    @Operation(summary = "Get theme by id", tags = "theme")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Requested theme",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json")
+                    })
+    })
     public ResponseEntity<ThemeDto> getTheme(@PathVariable("themeId") Long id) {
         return ResponseEntity.ok(themeService.getThemeById(id));
     }
 
     @PostMapping
+    @Operation(summary = "Create new theme", tags = "theme")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Theme created",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json")
+                    })
+    })
     public ResponseEntity<List<ThemeDto>> addNewThemes(@RequestBody List<Theme> themes) {
         return new ResponseEntity<>(themeService.addNewThemes(themes), HttpStatus.CREATED);
     }
 
     @DeleteMapping(path = "{themeId}")
+    @Operation(summary = "Delete theme by id", tags = "theme")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Theme has been deleted",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json")
+                    })
+    })
     public ResponseEntity<String> deleteTheme(@PathVariable("themeId") Long id) {
         themeService.deleteTheme(id);
         return ResponseEntity.ok("Тема удалена");
     }
 
     @PutMapping(path = "{themeId}")
+    @Operation(summary = "Update theme by id", tags = "theme")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Theme has been updated",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json")
+                    })
+    })
     public ResponseEntity<ThemeDto> updateTheme(@PathVariable("themeId") Long id,
                                                 @RequestBody Theme theme) {
         return ResponseEntity.ok(themeService.updateTheme(id, theme));
